70211c055e9ad6a3c281894d20918da201eaa22f
Evgen <3skif9@mail.ru>
2013-09-11 17:15:55 +0300

    added files from subfolder

---
+++ code/Result.php
@@ -0,0 +1,20 @@
+<?php
+
+class groupdocsViewer_Result extends ViewableData {
+	protected $width = false;
+	protected $height = false;
+	protected $guid = false;
+
+	
+	public function __construct($width, $height, $guid) {
+		$this->width = $width;
+		$this->height = $height;
+		$this->guid = $guid;
+		parent::__construct();
+	}
+	
+	public function forTemplate() {
+		return "<iframe src='http://apps.groupdocs.com/document-viewer/embed/" . $this->guid . "?&referer=silverstripe/1.0.0' frameborder='0' width='" . $this->width . "' height='" . $this->height ."'></iframe>";
+	}
+	
+}
---
+++ code/groupdocsViewer.php
@@ -0,0 +1,20 @@
+<?php
+
+class groupdocsViewer {
+	
+	public static function handle_shortcode($arguments, $url, $parser, $shortcode) {
+		$width = 600 ;
+		$height = 800;
+
+		if(isset($arguments['width']) && !empty($arguments['width'])) {
+			$width = $arguments['width'];
+		}
+
+		if(isset($arguments['height']) && !empty($arguments['height'])) {
+			$height = $arguments['height'];
+		}
+
+		$grpViewer = new groupdocsViewer_Result($width, $height, $url);
+		return $grpViewer->forTemplate();
+	}
+}
