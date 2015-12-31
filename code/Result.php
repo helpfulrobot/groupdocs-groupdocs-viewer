69a04899d801503da4d8db16ddaa5b5fb55c3dd0
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:18:45 +1300

    Converted to PSR-2

--- code/Result.php
+++ code/Result.php
@@ -1,20 +1,22 @@
 <?php
 
-class groupdocsViewer_Result extends ViewableData {
-	protected $width = false;
-	protected $height = false;
-	protected $guid = false;
+class groupdocsViewer_Result extends ViewableData
+{
+    protected $width = false;
+    protected $height = false;
+    protected $guid = false;
 
-	
-	public function __construct($width, $height, $guid) {
-		$this->width = $width;
-		$this->height = $height;
-		$this->guid = $guid;
-		parent::__construct();
-	}
-	
-	public function forTemplate() {
-		return "<iframe src='http://apps.groupdocs.com/document-viewer/embed/" . $this->guid . "?&referer=silverstripe/1.0.0' frameborder='0' width='" . $this->width . "' height='" . $this->height ."'></iframe>";
-	}
-	
+    
+    public function __construct($width, $height, $guid)
+    {
+        $this->width = $width;
+        $this->height = $height;
+        $this->guid = $guid;
+        parent::__construct();
+    }
+    
+    public function forTemplate()
+    {
+        return "<iframe src='http://apps.groupdocs.com/document-viewer/embed/" . $this->guid . "?&referer=silverstripe/1.0.0' frameborder='0' width='" . $this->width . "' height='" . $this->height ."'></iframe>";
+    }
 }
--- code/groupdocsViewer.php
+++ code/groupdocsViewer.php
@@ -1,20 +1,22 @@
 <?php
 
-class groupdocsViewer {
-	
-	public static function handle_shortcode($arguments, $url, $parser, $shortcode) {
-		$width = 600 ;
-		$height = 800;
+class groupdocsViewer
+{
+    
+    public static function handle_shortcode($arguments, $url, $parser, $shortcode)
+    {
+        $width = 600 ;
+        $height = 800;
 
-		if(isset($arguments['width']) && !empty($arguments['width'])) {
-			$width = $arguments['width'];
-		}
+        if (isset($arguments['width']) && !empty($arguments['width'])) {
+            $width = $arguments['width'];
+        }
 
-		if(isset($arguments['height']) && !empty($arguments['height'])) {
-			$height = $arguments['height'];
-		}
+        if (isset($arguments['height']) && !empty($arguments['height'])) {
+            $height = $arguments['height'];
+        }
 
-		$grpViewer = new groupdocsViewer_Result($width, $height, $url);
-		return $grpViewer->forTemplate();
-	}
+        $grpViewer = new groupdocsViewer_Result($width, $height, $url);
+        return $grpViewer->forTemplate();
+    }
 }
